8c5585a468e167577136093199f2a930fec03e96
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-05-12 18:18:18 +0300

    freecodecamp

--- JavaScript Algorithms and Data Structures/ES6/02_mutateAnArrayDeclaredWithConst.js
+++
@@ -1,9 +0,0 @@
-const s = [5, 7, 2];
-function editInPlace() {
-    // Only change code below this line
-
-    s.sort((a, b) => (a - b));
-
-    // Only change code above this line
-}
-editInPlace();
